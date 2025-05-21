c778985378e4b6e557f1af8d2db6a6edffaad10f
Aamir <aamirlacewala@gmail.com>
2025-05-21 01:38:52 -0400

    Initial Commit

---
+++ Cannes/CannesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CannesApp.swift
+//  Cannes
+//
+//  Created by Aamir Lacewala on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct CannesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CannesTests/CannesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CannesTests.swift
+//  CannesTests
+//
+//  Created by Aamir Lacewala on 5/21/25.
+//
+
+import Testing
+@testable import Cannes
+
+struct CannesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
