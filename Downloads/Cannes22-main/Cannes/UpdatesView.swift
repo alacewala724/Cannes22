0d25f08159c46e0bc573acb6410db2f2d5a52dfe
Aamir <aamirlacewala@gmail.com>
2025-07-25 19:26:01 -0400

    Remove shadow from FollowNotificationRow to eliminate visual gap

--- Downloads/Cannes22-main/Cannes/UpdatesView.swift
+++ Downloads/Cannes22-main/Cannes/UpdatesView.swift
@@ -377,7 +377,6 @@ struct FollowNotificationRow: View {
         .padding(.horizontal, 12)
         .background(Color(.systemBackground))
         .cornerRadius(16)
-        .shadow(color: .black.opacity(0.08), radius: 3, x: 0, y: 1)
         .onAppear {
             Task {
                 await checkFollowStatus()
